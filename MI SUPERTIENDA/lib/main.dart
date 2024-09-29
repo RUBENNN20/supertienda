56b1f8d04c9fc6d84844e552720c465d15cf51cb
RUBENNN20 <rdavidcs15@gmail.com>
2024-09-29 00:28:21 -0500

    main.dart

--- MI SUPERTIENDA/lib/main.dart
+++ MI SUPERTIENDA/lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart'; // Importar las pantallas
+import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
-import 'screens/home_page.dart'; // Importar las nuevas pantallas
+import 'screens/home_page.dart'; 
 import 'screens/categories_screen.dart';
 import 'screens/offers_screen.dart';
 import 'screens/supermarkets_screen.dart';
 import 'screens/cart_screen.dart';
 import 'screens/profile_screen.dart';
 
-void main() => runApp(MyApp());
+import 'screens2/supermercado1_screen.dart';
+import 'screens2/supermercado2_screen.dart';
+import 'screens2/supermercado3_screen.dart';
+import 'screens2/supermercado4_screen.dart';
+import 'screens2/supermercado5_screen.dart';
+import 'screens2/supermercado6_screen.dart';
+import 'screens2/supermercado7_screen.dart';
+import 'screens2/supermercado8_screen.dart';
+import 'screens2/supermercado9_screen.dart';
+import 'screens2/supermercado10_screen.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -35,6 +46,18 @@ class MyApp extends StatelessWidget {
         '/supermarkets': (context) => const SupermarketsScreen(),
         '/cart': (context) => const CartScreen(),
         '/profile': (context) => const ProfileScreen(),
+
+        // Rutas para los supermercados individuales
+        '/supermercado1': (context) => const Supermercado1Screen(),
+        '/supermercado2': (context) => const Supermercado2Screen(),
+        '/supermercado3': (context) => const Supermercado3Screen(),
+        '/supermercado4': (context) => const Supermercado4Screen(),
+        '/supermercado5': (context) => const Supermercado5Screen(),
+        '/supermercado6': (context) => const Supermercado6Screen(),
+        '/supermercado7': (context) => const Supermercado7Screen(),
+        '/supermercado8': (context) => const Supermercado8Screen(),
+        '/supermercado9': (context) => const Supermercado9Screen(),
+        '/supermercado10': (context) => const Supermercado10Screen(),
       },
     );
   }
