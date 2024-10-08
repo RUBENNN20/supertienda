207a5a0403e11e05fc339b7142913963ff20d194
RUBENNN20 <rdavidcs15@gmail.com>
2024-10-08 17:46:21 -0500

    sonarcloud

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
-import '../models/product.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
